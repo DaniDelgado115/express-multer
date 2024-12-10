e16ed638692f94e339ad50d1beb90a818fd95cbb
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-12-10 09:23:47 +0000

    a)

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const extension=file.originalname.split('.').pop()
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)+'.'+extension
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('form.html');
 });
+
+
 
 router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
